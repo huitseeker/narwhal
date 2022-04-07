0ca341e6c2d7c3eba4afb4ebcf368133f0089087
Anastasios Kichidis <akihidis@gmail.com>
2022-04-07 18:55:29 +0100

    feat: fix missing headers in files

--- primary/src/block_synchronizer/mod.rs
+++ primary/src/block_synchronizer/mod.rs
@@ -1,3 +1,5 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
 #![allow(dead_code)]
 use crate::{
     block_synchronizer::peers::Peers, primary::PrimaryMessage, utils, BatchDigest, Certificate,
--- primary/src/block_synchronizer/tests/block_synchronizer_tests.rs
+++ primary/src/block_synchronizer/tests/block_synchronizer_tests.rs
@@ -1,3 +1,5 @@
+// Copyright (c) 2022, Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
 use crate::{
     block_synchronizer::{
         BlockSynchronizer, CertificatesResponse, Command, RequestID, StateCommand, SyncError,
